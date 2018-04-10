c7afc0e69cd9c2b441f4ec9abe216e987fdb445d
alpenfritz <mymailismissing@gmail.com>
2018-04-11 01:46:56 +0900

    Dispatch deposit and withdraw actions to Wallet component + test

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { deposit, withdraw } from '../actions/balance';
 
 export class Wallet extends Component {
   constructor() {
@@ -12,15 +13,21 @@ export class Wallet extends Component {
     this.setState({ balance: parseInt(event.target.value, 10)});
   }
 
+  deposit = () => this.props.deposit(this.state.balance);
+
+  withdraw = () => this.props.withdraw(this.state.balance);
+
   render() {
     return (
       <div>
         <h3 className="balance">Wallet balance: {this.props.balance}</h3>
         <br />
         <input className="input-wallet" onChange={this.updateBalance} />
+        <button className="btn-deposit" onClick={this.deposit}>Deposit</button>
+        <button className="btn-withdraw" onClick={this.withdraw}>Withdraw</button>
       </div>
     );
   }
 }
 
-export default connect(state => { return { balance: state } })(Wallet);
+export default connect(state => { return { balance: state } }, { deposit, withdraw })(Wallet);
